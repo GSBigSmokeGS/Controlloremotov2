018ffebfd02c1a09983324df6104400f79ccbd3d
GSBigSmokeGS <153632788+GSBigSmokeGS@users.noreply.github.com>
2024-04-15 15:59:36 +0000

    Esercizi

---
+++ Esercizio_A.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# parte 1
+# tail -1 prova.txt
+
+# parte 3
+#cat prova.txt | tr '[:lower:]' '[:upper:]'
+
+# parte 4
+#es4=$(cat prova.txt | cut -d, -f 1 )
+#echo $es4
+
+# parte 5
+#comando=$(tail -1 prova2.txt)
+#eval "$comando"
